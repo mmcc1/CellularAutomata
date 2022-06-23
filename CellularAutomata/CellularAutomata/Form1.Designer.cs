b18a31fbb3ab4f39c088abfb783de596065dbec8
Marx Bitware <info@marxbitware.co.uk>
2022-06-23 16:43:45 +0700

    Minor bug fix setting lower limit of Death bias to 2.

--- CellularAutomata/CellularAutomata/Form1.Designer.cs
+++ CellularAutomata/CellularAutomata/Form1.Designer.cs
@@ -103,7 +103,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Start";
+            this.button1.Text = "&Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -125,6 +125,11 @@
             this.numericUpDown3.Location = new System.Drawing.Point(6, 81);
             this.numericUpDown3.Maximum = new decimal(new int[] {
             2147483647,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            2,
             0,
             0,
             0});
@@ -244,7 +249,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
-            this.Text = "Cellular Automata v1.0.3";
+            this.Text = "Cellular Automata v1.0.4";
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
